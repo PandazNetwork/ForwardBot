6b38a72b7333b0ad970b35573af92f207956d1f0
PandazNetwork <156095336+PandazNetwork@users.noreply.github.com>
2024-01-12 23:26:16 +0530

    typo error fixed

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get('API_ID', '1455678'))
-API_HASH = environ.get('API_HASH', 'a6b02f54dab5607e542eb216f28a1e6d')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6635160184:AAFp4FhPN5Lkk8BIC8kLuZu9smqkDka57tQ')
+API_ID = int(environ.get('API_ID', ''))
+API_HASH = environ.get('API_HASH', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
 FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
-OWNER = (environ.get('OWNER', '6769157231'))
+OWNER = (environ.get('OWNER', ''))
 PRIVATE_BOT = (environ.get('PRIVATE_BOT', False))
 UPDATES_CHANNEL = environ.get('UPDATES_CHANNEL', 'https://t.me/webverseupdates')
 SUPPORT_GROUP = environ.get('SUPPORT_GROUP', 'https://t.me/webversegroup')
