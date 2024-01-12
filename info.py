1ccf897824d1654dc03a6689d628e2ead66fe368
PandazNetwork <156095336+PandazNetwork@users.noreply.github.com>
2024-01-12 23:33:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,13 +1,13 @@
 from os import environ
 
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
-FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
-OWNER = (environ.get('OWNER', ''))
-PRIVATE_BOT = (environ.get('PRIVATE_BOT', False))
-UPDATES_CHANNEL = environ.get('UPDATES_CHANNEL', 'https://t.me/webverseupdates')
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', 'https://t.me/webversegroup')
-DEV_LINK = environ.get('DEV_LINK', 'https://t.me/bhaiyajixbot')
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+FILE_CAPTION = environ.get("FILE_CAPTION", "<code>{file_name}</code>")
+OWNER = (environ.get("OWNER", ""))
+PRIVATE_BOT = (environ.get("PRIVATE_BOT", False))
+UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL", "https://t.me/webverseupdates")
+SUPPORT_GROUP = environ.get("SUPPORT_GROUP", "https://t.me/webversegroup")
+DEV_LINK = environ.get("DEV_LINK", "https://t.me/bhaiyajixbot")
 
 # customise as per your need
