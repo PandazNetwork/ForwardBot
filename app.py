b5b65dda6dccaef3ccd9d39b6c838292a8419eef
PandazNetwork <156095336+PandazNetwork@users.noreply.github.com>
2024-11-22 08:47:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Back4apper!"
+    return "Hello PandazNetwork !"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1337)#8080
+    app.run(host='0.0.0.0', port=1337) #8080
